9f4bb07ba1677f8853560e79254aee0250c87702
rena204 <rena2004.rr@gamil.com>
2023-06-09 20:00:37 +0400

    lesson4

---
+++ main/java/ru/geekbrains/lesson4/homework/Fruit.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.lesson4.homework;
+
+public abstract  class Fruit {
+
+
+    private final float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+}
